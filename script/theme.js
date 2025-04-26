baa9bc1306d9de35cdd6ad487f525f448809f765
NAHIDA SULTANA <nahidas705@gmail.com>
2025-04-26 22:54:49 +0600

    added all

--- script/theme.js
+++ script/theme.js
@@ -1,6 +1,9 @@
+const color_buttin=["bg-red-300", "bg-yellow-400" ,"bg-green-200","bg-cyan-100","bg-blue-200","bg-purple-300","bg-pink-300","bg-pink-800"]
+let item=0;
+
 document.getElementById("color").addEventListener('click', function(){
+document.body.classList.remove(...color_buttin)
+document.body.classList.add(color_buttin[item]) 
 
-document.body.style.backgroundColor="bg-red-200"
-
-
+item+=1%color_buttin.length
 })
